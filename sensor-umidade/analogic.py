1d0ddb193f28d3fdfc485a9792e527e6bc17590b
alegume <alexandreabreu@comp.ufla.br>
2019-05-28 18:09:49 -0300

    Codigo simplificado

--- sensor-umidade/analogic.py
+++ sensor-umidade/analogic.py
@@ -7,14 +7,13 @@ ADC.setup(0x48)
 def loop():
 	while True:
 		print(ADC.read(0))
-		# ADC.write(ADC.read(0))fetch
+		# ADC.write(ADC.read(0))
 
 def destroy():
 	ADC.write(0)
 
 if __name__ == "__main__":
 	try:
-		setup()
 		loop()
 	except KeyboardInterrupt:
 		destroy()
--- sensor-umidade/umidade-analogic-output.py
+++ sensor-umidade/umidade-analogic-output.py
@@ -3,14 +3,17 @@
 import smbus
 import time
 
+#check your PCF8591 address by type in 'sudo i2cdetect -y -1' in terminal.
 address = 0x48
 A0 = 0x40
 A1 = 0x41
 A2 = 0x42
 A3 = 0x43
+# for RPI version 1, use "bus = smbus.SMBus(0)"
 bus = smbus.SMBus(1)
+
 while True:
-	bus.write_byte(address,A0)
+	bus.write_byte(address, A0)
 	value = bus.read_byte(address)
 	print('AOUT:{}  '.format(value * 3.3 / 255))
 	time.sleep(0.2)
