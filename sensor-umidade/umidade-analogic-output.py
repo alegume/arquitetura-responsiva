41f4d357e68c0a2c6cbbb1b57568b019126b531d
alegume <alexandreabreu@comp.ufla.br>
2019-05-28 16:06:31 -0300

    entrada digital adicionada

---
+++ sensor-umidade/umidade-analogic-output.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import smbus
+import time
+
+address = 0x48
+A0 = 0x40
+A1 = 0x41
+A2 = 0x42
+A3 = 0x43
+bus = smbus.SMBus(1)
+while True:
+	bus.write_byte(address,A0)
+	value = bus.read_byte(address)
+	print('AOUT:{}  '.format(value * 3.3 / 255))
+	time.sleep(0.2)
