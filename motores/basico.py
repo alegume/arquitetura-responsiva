359bf1ea58a420da7a4123c365de6e11a41317d2
alegume <alexandreabreu@comp.ufla.br>
2019-10-25 20:23:41 -0300

    s

--- motores/basico.py
+++ motores/basico.py
@@ -6,7 +6,8 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(5) # Initialization
+x = input('start:\n')
+p.start(float(5)) # Initialization
 try:
   while True:
     x = input('duty cycle:\n')
