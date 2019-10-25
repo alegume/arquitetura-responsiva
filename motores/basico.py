ce90de08622e51299f6a7eec3b32241146b16e81
alegume <alexandreabreu@comp.ufla.br>
2019-10-25 19:51:31 -0300

    basico

--- motores/basico.py
+++ motores/basico.py
@@ -6,25 +6,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p.start(5) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+    x = input('duty cycle')
+    p.ChangeDutyCycle(x)
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
