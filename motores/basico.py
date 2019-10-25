af8a87b19abb119b1904542720bd6a540eebc4f3
alegume <alexandreabreu@comp.ufla.br>
2019-10-25 20:03:29 -0300

    a

--- motores/basico.py
+++ motores/basico.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-p.stop()
 GPIO.cleanup()
 
 servoPIN = 18
@@ -9,6 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.stop()
 p.start(5) # Initialization
 try:
   while True:
