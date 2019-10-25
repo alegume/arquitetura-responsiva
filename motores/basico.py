7f90dd99a5ab73d1bbbfc24c5696647480865e61
alegume <alexandreabreu@comp.ufla.br>
2019-10-25 20:39:20 -0300

    bcm

--- motores/basico.py
+++ motores/basico.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 11
-GPIO.setmode(GPIO.BOARD)
+servoPIN = 18
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
