7d10f55a6a5dfc8aac9868e0a841cd18c3cc487d
alegume <alexandreabreu@comp.ufla.br>
2019-10-25 19:52:28 -0300

    a

--- motores/basico.py
+++ motores/basico.py
@@ -9,8 +9,8 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(5) # Initialization
 try:
   while True:
-    x = input('duty cycle')
-    p.ChangeDutyCycle(x)
+    x = input('duty cycle:\n')
+    p.ChangeDutyCycle(float(x))
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
